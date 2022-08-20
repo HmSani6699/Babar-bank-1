dae425efd01b11d2055123497581fa5bd7a29893
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2022-08-20 00:44:24 -0700

    ami vallo achi

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.getElementById('submit-button').addEventListener('click', function () {
+    // get name input
+    const nameInput = document.getElementById('name-input');
+    const nameInputValue = nameInput.value;
+    //get email input
+    const passwordInput = document.getElementById('passwor-input');
+    const passwordInputValue = passwordInput.value;
+    if (nameInputValue == 'mdsani' && passwordInputValue == '12345') {
+        window.location.href = 'bank.html'
+    }
+
+
+})
