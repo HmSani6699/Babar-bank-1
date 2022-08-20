881ef285dce936cb0fe19611fc6ac55335435a2d
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2022-08-20 10:20:37 -0700

    fainal part

--- js/bank.js
+++ js/bank.js
@@ -1,42 +1,47 @@
-document.getElementById('deposit-button').addEventListener('click', function () {
-    //get deposit input
-    const depositInput = document.getElementById('deposit-input');
+function getInput(input) {
+    const depositInput = document.getElementById(input);
     const depositInputValue = depositInput.value;
     const depositNumber = parseFloat(depositInputValue);
-    //update deposit total amount
-    const depositTotal = document.getElementById('deposit-total');
+    //get clear deposit input
+    depositInput.value = '';
+    return depositNumber;
+}
+
+function getTotalAmount(depositNumber, amount) {
+    const depositTotal = document.getElementById(amount);
     const depositTotalAmount = depositTotal.innerText;
     const previousDepositTotalAmount = parseFloat(depositTotalAmount);
     depositTotal.innerText = previousDepositTotalAmount + depositNumber;
+}
 
-    //get update balance
+function updateBalance(depositNumber, condition) {
     const balanceText = document.getElementById('balance-total');
     const balancetotal = balanceText.innerText;
     const previousBalanceTotal = parseFloat(balancetotal);
-    balanceText.innerText = previousBalanceTotal + depositNumber;
+    if (condition == true) {
+        balanceText.innerText = previousBalanceTotal + depositNumber;
+    }
+    else {
+        balanceText.innerText = previousBalanceTotal - depositNumber;
+    }
 
-    //get clear deposit input
-    depositInput.value = '';
+}
+document.getElementById('deposit-button').addEventListener('click', function () {
+
+    const depositNumber = getInput('deposit-input');
+    if (depositNumber > 0) {
+        getTotalAmount(depositNumber, 'deposit-total');
+        updateBalance(depositNumber, true);
+    }
 });
 
 document.getElementById('witdrow-button').addEventListener('click', function () {
-    //get witdrow input
-    const witdrowinput = document.getElementById('witdrow-input');
-    const witdroInputValue = witdrowinput.value;
-    const witdrowNumber = parseFloat(witdroInputValue);
-    //get update witdrow total amount
-    const witdrowTotal = document.getElementById('witdrow-total');
-    const witdrowTotalAmount = witdrowTotal.innerText;
-    const previouswitdeowAmount = parseFloat(witdrowTotalAmount);
-    witdrowTotal.innerText = previouswitdeowAmount + witdrowNumber;
-
-    //get update balance
+    const witdrowNumber = getInput('witdrow-input');
     const balanceText = document.getElementById('balance-total');
     const balancetotal = balanceText.innerText;
     const previousBalanceTotal = parseFloat(balancetotal);
-    balanceText.innerText = previousBalanceTotal - witdrowNumber;
-
-    //clear witdrow input
-    witdrowinput.value = '';
-
-})
+    if (witdrowNumber > 0 && witdrowNumber < previousBalanceTotal) {
+        getTotalAmount(witdrowNumber, 'witdrow-total');
+        updateBalance(witdrowNumber, false);
+    }
+});
